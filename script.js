e44a24cd6d58401f7da66ce9b08a4a4ee95f11b9
thesunlace <gcmelo37768801@gmail.com>
2024-03-23 18:17:17 -0300

    Added and stylized a navigation bar.

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ document.getElementById("cards").onmousemove = e => {
     card.style.setProperty("--mouse-y", `${y}px`);
     }
 }
+
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+
+    if(currentScroll <= 0) {
+        body.classList.remove("scroll-up");
+    }
+
+    if(currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up");
+        body.classList.add("scroll-down");
+    }
+
+    if(currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down");
+        body.classList.add("scroll-up");
+    }
+
+
+    lastScroll = currentScroll;
+})
